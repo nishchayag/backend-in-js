74d0d8ecb4d59a49894fb273ab49cfe948ace653
Nishchay Agarwal <nishchay.agar@gmail.com>
2025-06-05 12:32:47 +0530

    connect to mongo atlas and compass

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// import express from "express";
+// const app = express.Router();
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Home Page </h1>");
+// });
+
+// app.listen(process.env.PORT, () => {
+//   console.log("Server Started");
+// });
